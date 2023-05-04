5638f70df8bcc7de8393078ec229cee48200d420
B15#4L <64602552+abbishal@users.noreply.github.com>
2023-05-05 00:25:59 +0600

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 shopt -s expand_aliases
 source ~/.bash_alias
-set -e
 figlet "MRX_B15H4L" | lolcat
 
 net(){
     ping -c 1 google.com &>/dev/null
-    while [ $? -ne 0 ]; do
-        echo "Network problem occured"
-        sleep 60
-        echo "Checking Network again...."
-        ping -c 1 google.com &>/dev/null
-    done
+    if [ $? -ne 0 ]; then
+        printf "Network problem identified." >> /var/www/html/sc.html
+        while [ $? -ne 0 ]; do
+            sleep 60
+            printf "." >> /var/www/html/sc.html
+            ping -c 1 google.com &>/dev/null
+        done
+    fi
 }
 checking() {
     echo "Checking if dependencies installed........" 
@@ -67,17 +68,15 @@ if [ $# -ne 1 ]
     echo "Usage: $0 subdomain_list"
     exit
 fi
-if [ -d "~/recon/$1" ]; then
+if [ -d ~/"recon/$1" ]; then
     echo "Directory Exist Maybe already scanned....."
 else
     echo "Creating Directory....."
     mkdir ~/recon/$1/
     mkdir ~/recon/$1/upload
-    mkdir /var/www/html/results/$1
 fi
 if [ ! -f $1 ]; then
     echo "subdomain_list not exist"
-    rm -r ~/recon/$1
     exit
 else
     cp $1 ~/recon/$1/target_list
@@ -91,14 +90,14 @@ split -l 5 --additional-suffix=.split target_list
 for s in ~/recon/$1/*.split
 do
     findom $s
-    net
+    #net
 done
 rm *.split
 sort *.txt -u | puredns resolve -r ~/wordlist/resolvers.txt --write "$1.fd" &>/dev/null &
 echo "Resolving findomain subdomains....."
 wait
 sort "$1.fd" > "$1.new0"
-rm *.tmp
+rm *.txt
 echo "Resolving done."
 counter=0
 new=$(wc -l < "$1.new$counter")
@@ -107,20 +106,27 @@ printf "%d New Subdomains Found\n" $new
 while [ $new -ge 1 ]; do
     ((counter=counter+1))
     printf "Bruteforce level %d starting.....\n" $counter
-    split -l 10 --additional-suffix=.split "$1.new$(($counter-1))"
+    split -l 1 --additional-suffix=.split "$1.new$(($counter-1))"
+    total=$(sort "$1.new$(($counter-1))" | wc -l)
+    scanned=0
     for s in ~/recon/$1/*.split
     do
         bruteforce $s
-        net
+        ((scanned=scanned+2))
+        printf "<b>Bruteforce level %d Running.</b><br>Total %d Domains scanned out of %d<br>" $counter $scanned $total > /var/www/html/sc.html
+        #net
     done
     sort *.tmp -u > "$1.brute$counter"
     rm *.tmp
 
+    scanned=0
     printf "DnsGen level %d starting.....\n" $counter
     for s in ~/recon/$1/*.split
     do
         dnsg $s
-        net
+        ((scanned=scanned+2))
+        printf "<b>DnsGen level %d Running.</b><br>Total %d Domains scanned out of %d<br>" $counter $scanned $total > /var/www/html/sc.html
+        #net
     done
     sort *.tmp -u > "$1.gen$counter"
     printf "DnsGen level %d Completed.\n" $counter
@@ -132,19 +138,7 @@ while [ $new -ge 1 ]; do
     printf "%d New Subdomains Found\n" $new
     
 done
-
+printf "Network problem identified." >> /var/www/html/sc.html
 sort "$1.new"* -u > "upload/$1.subdomain.txt"
+cp "upload/$1.subdomain.txt" "/var/www/html/$1.subdomain.txt"
 curl "https://api.telegram.org/bot6102545432:AAHLTE0SdQ7neK5Q2D2gmrGheDJc8ew6uN8/sendMessage?chat_id=847743133&text=Subdomain Scan Completed for $1 now scanning for low-hanging bugs."
-echo "checking for possible takeovers....."
-dnsreaper "upload/$1.subdomain.txt" > "upload/$1.dnsreaper.txt" &
-takeover -l "upload/$1.subdomain.txt" -o "upload/$1.takeover.txt" &>/dev/null &
-wait
-
-echo "Starting Nuclei....."
-naabu -l "upload/$1.subdomain.txt" -silent -o "upload/$1.naabu.txt"
-nuclei -l "upload/$1.naabu.txt" -t mrx -o "upload/$1.nuclei.txt" -silent &>/dev/null
-
-zip $1 upload/$1.*
-mv $1.zip /var/www/html/results
-cp -R upload/$1.* /var/www/html/results/$1
-curl "https://api.telegram.org/bot6102545432:AAHLTE0SdQ7neK5Q2D2gmrGheDJc8ew6uN8/sendMessage?chat_id=847743133&text=Scan with nuclei Completed and files uploaded for $1."
